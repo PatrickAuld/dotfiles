3e7852a75480ec571fa4ca557c761e268822dba0
Patrick Auld <patrick@patrickauld.com>
2021-07-20 13:42:41 -0700

    Adds Emojicomplete Hammerspoon to install script

--- hammerspoon/install.sh
+++ hammerspoon/install.sh
@@ -3,10 +3,15 @@
 if [[ ! -a ~/.hammerspoon/anycomplete ]]; then
   echo "Installing Hammerspoon anycomplete scripts"
   git clone https://github.com/nathancahill/anycomplete.git ~/.hammerspoon/anycomplete
-  echo "local anycomplete = require \"anycomplete/anycomplete\"" >> ~/.hammerspoon/init.lua
-  echo "anycomplete.registerDefaultBindings()" >> ~/.hammerspoon/init.lua
 else
   echo "Skipping installing Hammerspoon anycomplete"
+fi
+
+if [[ ! -a ~/.hammerspoon/emojicomplete ]]; then
+  echo "Installing Hammerspoon emojicomplete scripts"
+  git clone https://github.com/PatrickAuld/emojicomplete.git ~/.hammerspoon/emojicomplete
+else
+  echo "Skipping installing Hammerspoon emojicomplete"
 fi
 
 echo "Opening Hammerspoon App, reload the configs"
