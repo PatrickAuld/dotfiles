9190002b893a0d856bd0076c0549612a31f05ad6
Patrick Auld <patrick@patrickauld.com>
2023-01-05 10:57:55 -0800

    Adds new Mac defaults and organizes them by application

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -1,17 +1,32 @@
 # Sets reasonable macOS defaults.
 #
-# Or, in other words, set shit how I like in macOS.
-#
 # The original idea (and a couple settings) were grabbed from:
 #   https://github.com/mathiasbynens/dotfiles/blob/master/.macos
+#   https://macos-defaults.com/
 #
 # Run ./set-defaults.sh and you'll be good to go.
 
+#####
+# Keyboard
+#
 # Disable press-and-hold for keys in favor of key repeat.
-defaults write -g ApplePressAndHoldEnabled -bool false
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
+# Set a really fast key repeat.
+defaults write NSGlobalDomain KeyRepeat -int 1
+
+#####
+# Airdrop
+#
 # Use AirDrop over every interface. srsly this should be a default.
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
+
+#####
+# Finder
+#
+# Set the Finder prefs for showing a few different volumes on the Desktop.
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 
 # Always open everything in Finder's list view. This is important.
 defaults write com.apple.Finder FXPreferredViewStyle Nlsv
@@ -19,17 +34,22 @@ defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 # Show the ~/Library folder.
 chflags nohidden ~/Library
 
-# Set a really fast key repeat.
-defaults write NSGlobalDomain KeyRepeat -int 1
+#####
+# Dock
+#
+# Sleep if we're in the bottom-right hot corner.
+defaults write com.apple.dock wvous-br-corner -int 10
+defaults write com.apple.dock wvous-br-modifier -int 0
 
-# Set the Finder prefs for showing a few different volumes on the Desktop.
-defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
-defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+# Always set the Dock to autohide
+defaults write com.apple.dock autohide -bool true
 
-# Run the screensaver if we're in the bottom-left hot corner.
-defaults write com.apple.dock wvous-bl-corner -int 5
-defaults write com.apple.dock wvous-bl-modifier -int 0
+# Choose whether to rearrange Spaces automatically
+defaults write com.apple.dock mru-spaces -bool false
 
+#####
+# Safari
+#
 # Hide Safari's bookmark bar.
 defaults write com.apple.Safari ShowFavoritesBar -bool false
 
@@ -39,3 +59,10 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+#####
+# TextEdit
+#
+# Have TextEdit open a new file and launch instead of a open dialogue
+defaults write com.apple.TextEdit NSShowAppCentricOpenPanelInsteadOfUntitledFile -bool false
+
