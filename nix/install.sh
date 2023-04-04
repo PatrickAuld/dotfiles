38feb5980754914212ac63e0c535ded2eb8aaf8a
Patrick Auld <patrick@patrickauld.com>
2023-04-04 13:58:35 -0700

    Corrects Nix install for MacOS

--- nix/install.sh
+++ nix/install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# https://nixos.org/download.html#nix-install-macos
+
 echo "Installing Nix multi-user..."
-sh <(curl -L https://nixos.org/nix/install) --daemon
+sh <(curl -L https://nixos.org/nix/install)
 echo "Nix installed"
