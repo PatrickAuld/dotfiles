c6991a40cc85c361e86dc3dde38260558d653bc6
Patrick Auld <patrick@patrickauld.com>
2025-07-14 15:25:36 -0700

    Lazy load nvm utils. This saves ~100ms of shell startup

--- nvm/config.zsh
+++ nvm/config.zsh
@@ -1,5 +1,35 @@
 # install as defined here: https://github.com/nvm-sh/nvm#install--update-script
 
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+# Lazy load NVM for faster shell startup
+_load_nvm() {
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+}
+
+# Create lazy-loaded wrapper functions for NVM commands
+nvm() {
+    unset -f nvm node npm npx
+    _load_nvm
+    nvm "$@"
+}
+
+node() {
+    unset -f nvm node npm npx
+    _load_nvm
+    node "$@"
+}
+
+npm() {
+    unset -f nvm node npm npx
+    _load_nvm
+    npm "$@"
+}
+
+npx() {
+    unset -f nvm node npm npx
+    _load_nvm
+    npx "$@"
+}
 
