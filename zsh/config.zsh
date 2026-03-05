6f19a2be6b345fbd87a92937a585b267a8b2718e
Patrick Auld <patrick@patrickauld.com>
2026-03-05 15:12:57 -0800

    Moves zsh config into config.zsh

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -33,3 +33,12 @@ setopt HIST_REDUCE_BLANKS
 setopt complete_aliases
 
 set -o emacs
+
+# Better history
+# Credits to https://coderwall.com/p/jpj_6q/zsh-better-history-searching-with-arrow-keys
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search # Up
+bindkey "^[[B" down-line-or-beginning-search # Dow
