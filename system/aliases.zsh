9454bc54c7ec8338924d112f0db88f767f401f2a
Patrick Auld <patrick@patrickauld.com>
2026-04-02 22:34:59 +0000

    system: use ls when gls is unavailable

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,8 +1,12 @@
-# grc overides for ls
-#   Made possible through contributions from generous benefactors like
-#   `brew install coreutils`
+# Prefer GNU ls (`gls`) when installed via coreutils on macOS; otherwise use
+# the system `ls` (e.g. GNU ls on Linux).
+if command -v gls >/dev/null 2>&1; then
+  LS_BIN='gls'
+else
+  LS_BIN='ls'
+fi
 
-alias ls="gls -F --color"
-alias l="gls -lAh --color"
-alias ll="gls -l --color"
-alias la='gls -A --color'
+alias ls="$LS_BIN -F --color"
+alias l="$LS_BIN -lAh --color"
+alias ll="$LS_BIN -l --color"
+alias la="$LS_BIN -A --color"
